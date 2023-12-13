21ce40bfceb9ad0124e68574b44ddbe83bed3e26
miike1232 <miklmihno@gmail.com>
2023-12-13 14:58:58 +0200

    new

--- main.js
+++ main.js
@@ -22,10 +22,18 @@ document.addEventListener("keydown", event => {
 const userButtons = document.querySelectorAll(".chat-container .chat-link")
 userButtons.forEach((element) => {
     element.addEventListener("click", (element) => {
+        const messageContainer = document.querySelector("main.message-container")
+        messageContainer.style.display = "flex"
         const userName = element.target.querySelector(".chat-name").innerText
         const userLogoSrc = element.target.querySelector(".chat-logo").getAttribute("src")
         const messageUserBlock = document.querySelector(".message-container .user-block")
         messageUserBlock.querySelector(".user-logo").setAttribute("src", userLogoSrc)
         messageUserBlock.querySelector(".user-name").innerText = userName
     })
+})
+
+const buttonPageUsers = document.querySelector(".chat-container .users-view")
+buttonPageUsers.addEventListener("click", () => {
+    const messageContainer = document.querySelector("main.message-container")
+    messageContainer.style.display = "none"
 })
